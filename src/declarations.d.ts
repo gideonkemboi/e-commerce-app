8269e90e027d029a31b373bf6e66f042dccc0c24
Gideon Kemboi <gedionkemboi18@gmail.com>
2025-05-16 16:09:05 +0300

    feat: add assets and type declarations
    - Added various assets including images and SVGs for the application.
    - Added `declarations.d.ts` for type declarations.

---
+++ src/declarations.d.ts
@@ -0,0 +1,11 @@
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.jpg";
